4274f37bec3eeb0b27a3ddcdf57f9a709852cc84
David Carmo <david.santos@fcamara.com.br>
2021-11-30 11:29:14 -0300

    teste checked exception bytebank

--- bytebank-herdado-conta/src/TesteSaca.java
+++ bytebank-herdado-conta/src/TesteSaca.java
@@ -3,8 +3,13 @@ public class TesteSaca {
 	public static void main(String[] args) {
 		Conta conta  = new ContaCorrente(123, 321);
 		conta.deposita(200.0); 
+		try {
 		conta.saca(210.0);
-		
+		}
+		catch(Exception ex)
+		{
+			System.out.println("Ex: " + ex.getMessage());
+		}
 		System.out.println("Seu novo saldo: " + conta.getSaldo());
 	}
 }
